092a2608c6819a6bc49b77ead3798aff39b57679
AkashAhmed <akash.ahmed@orbitax.com>
2023-06-05 19:25:14 +0600

    caching added in member service

--- FrontendPart/src/app/_interceptors/loading.interceptor.ts
+++ FrontendPart/src/app/_interceptors/loading.interceptor.ts
@@ -17,7 +17,7 @@ export class LoadingInterceptor implements HttpInterceptor {
     this.loadingIndicatorService.requestIncoming();
 
     return next.handle(request).pipe(
-      delay(1000),
+      delay(800),
       finalize(() => {
         this.loadingIndicatorService.requestCompleted()
       })
