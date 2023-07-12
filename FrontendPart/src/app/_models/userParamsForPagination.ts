774846c45564cb3db41e8099513611a9f180480f
AkashAhmed <akash.ahmed@orbitax.com>
2023-07-12 19:23:10 +0600

    client side sorting functionality implemented

--- FrontendPart/src/app/_models/userParamsForPagination.ts
+++ FrontendPart/src/app/_models/userParamsForPagination.ts
@@ -6,6 +6,7 @@ export class UserParamsForPagination {
     gender: string;
     minAge = 18;
     maxAge = 99;
+    orderBy = 'lastActive';
 
     constructor (user: User) {
         this.gender = user.gender === 'male' ? 'female' : 'male';
